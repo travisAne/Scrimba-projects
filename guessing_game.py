fcc953097ce52e0d331b17a62c32e6fbbd550225
Travis <travispadera@gmail.com>
2023-10-09 18:39:26 +0200

    guessing_game

---
+++ guessing_game.py
@@ -0,0 +1,20 @@
+num = 76
+guess = 0
+guess_limit=5
+guess_number = 0
+guess = int(input(f'Guess a number 1-100: '))
+guess_number +=1
+while guess_number < guess_limit:
+    
+    if guess != num:
+        guess_number +=1
+        if guess > num:
+            guess = int(input(f'{guess} Too high - Guess again 1-100: '))
+        else:
+            guess = int(input(f'{guess} too low - Guess again 1-100: '))
+    if guess == num:
+        print(f'You Win! You Guessed it: {guess}')
+        break
+    
+if guess != num:
+    print(f'Sorry you lose! It was {num}')
